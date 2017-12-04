cd4229c7ee2a4f65b683d58a600b7fc66908081c
Dileep Rajput <dileep_rajput@yahoo.com>
2017-12-04 09:13:02 -0500

    Add basic logging

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let Game = require('./game');
 let GameObjects = require('./gameObjects');
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 let game;
 
 let app = express();
